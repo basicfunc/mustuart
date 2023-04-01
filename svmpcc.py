3e09b0b5956642d87c4b071d5b11f8aaf54790a5
Rahul <basicfunc@gmail.com>
2023-04-01 11:48:38 +0530

    Improvements

--- svmpcc.py
+++ svmpcc.py
@@ -1,20 +1,28 @@
-import os
-import numpy as np
-from scipy.io import wavfile
-from python_speech_features import mfcc
-from sklearn.model_selection import train_test_split
-from sklearn import svm
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import StandardScaler
-import pickle
 import sys
+import pickle
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from scipy.io import wavfile
+import numpy as np
+import os
 
 
-def load_song(filename):
+def load_song(filename, pca):
     rate, signal = wavfile.read(filename)
-    mfcc_features = mfcc(signal, rate, numcep=20)
-    mfcc_features = mfcc_features.mean(axis=0)
-    return mfcc_features.reshape(1, -1)
+    signal = signal.astype('float32') / 32767  # normalize to [-1, 1]
+    # zero-pad to multiple of 512
+    signal = np.pad(signal, (0, 512 - signal.shape[0] % 512))
+    frames = signal.reshape(-1, 512)
+    features = []
+    for frame in frames:
+        # apply PCA to each frame
+        feature = pca.transform(frame.reshape(1, -1))
+        feature = feature.mean(axis=0)
+        features.append(feature)
+    return np.mean(features, axis=0).reshape(1, -1)
 
 
 if len(sys.argv) != 2:
@@ -34,6 +42,10 @@ if os.path.exists('models/svm_model.pkl') and \
     data = np.load('models/data.npy')
     labels = np.load('models/labels.npy')
 
+    # Apply PCA to the data
+    pca = PCA(n_components=20)
+    data = pca.fit_transform(data)
+
     X_train, X_test, y_train, y_test = train_test_split(
         data, labels, test_size=0.2, random_state=42)
 
@@ -43,7 +55,7 @@ if os.path.exists('models/svm_model.pkl') and \
     X_test = scaler.transform(X_test)
 
     # Load a song to predict its genre
-    mfcc_features = scaler.transform(load_song(song_path))
+    mfcc_features = scaler.transform(load_song(song_path, pca))
 
     # Predict the genre of the song
     genre = clf.predict(mfcc_features)
@@ -52,13 +64,13 @@ if os.path.exists('models/svm_model.pkl') and \
 
 else:
     # Set the path to the directory containing the audio files
-    audio_dir = 'dataset/genres'
+    audio_dir = 'datasets/genres'
 
     # Define the list of genres
     genres = ['blues', 'classical', 'country', 'disco',
               'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
 
-    # Load the audio data and extract MFCC features for each file
+    # Load the audio data and extract PCA features for each file
     data = []
     labels = []
     for genre in genres:
@@ -66,14 +78,21 @@ else:
         for filename in os.listdir(genre_dir):
             filepath = os.path.join(genre_dir, filename)
             rate, signal = wavfile.read(filepath)
-            mfcc_features = mfcc(signal, rate, numcep=20)
-            mfcc_features = mfcc_features.mean(axis=0)
-            data.append(mfcc_features)
-            labels.append(genre)
+            signal = signal.astype('float32') / 32767  # normalize to [-1, 1]
+            # zero-pad to multiple of 512
+            signal = np.pad(signal, (0, 512 - signal.shape[0] % 512))
+            frames = signal.reshape(-1, 512)
+            for frame in frames:
+                data.append(frame)
+                labels.append(genre)
 
     # Convert the data and labels to NumPy arrays
     data = np.array(data)
     labels = np.array(labels)
+
+    # Apply PCA to the data
+    pca = PCA(n_components=20)
+    data = pca.fit_transform(data)
 
     np.save('models/data.npy', data)
     np.save('models/labels.npy', labels)
@@ -105,8 +124,6 @@ else:
     print("Accuracy:", accuracy)
 
     # Load a song to predict its genre
-    mfcc_features = scaler.transform(load_song(song_path))
+    mfcc_features = scaler.transform(load_song(song_path, pca))
 
     # Predict the genre of the song
-    genre = clf.predict(mfcc_features)
-    print("The genre of the song is", genre[0])
